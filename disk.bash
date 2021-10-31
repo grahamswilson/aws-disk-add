e5958473b883ad7c6eeb941849a8b30391eaf321
root <root@ip-172-31-55-251.ec2.internal>
2021-10-31 22:23:34 +0000

    Continued work on disk.bash

--- disk.bash
+++ disk.bash
@@ -2,6 +2,100 @@
 
 set -xv
 
+add_new_disk () {
+
+# Set SIZE to be the parameter passed to this function
+SIZE=$1
+
+# Get a list of current devices seen by the OS for later comparison after new disk is added
+CURRENTDISKS=($(lsblk -a | grep -v ^NAME | grep -v ^'├' | grep -v ^'└' | awk '{print $1}'))
+
+echo ${CURRENTDISKS[@]}
+
+# Get my AWS Instance-ID
+INSTANCE=$(curl -q -s http://169.254.169.254/latest/meta-data/instance-id)
+
+$ Get what AWS Region I am in
+REGION=$(curl -q -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+
+$ Get a list of current AWS volumes I have
+volumes=($(/usr/local/bin/aws ec2 describe-volumes --region ${REGION%?} --filters "Name=attachment.instance-id,Values=$INSTANCE" | grep \"Device\"\:  | awk '{print $2}' | sed 's/[",]//g'))
+
+echo ${volumes[*]}
+
+# Looping through possible new AWS volume device names searching for the first one that isn't in my current list of attached volumes
+for newdiskname in f g h i j k l m n o p q r s t u v w x z y
+do
+
+if [[ " ${volumes[*]} " =~ " /dev/sd${newdiskname} " ]];
+then
+        echo /dev/sd${newdiskname} is already attached to this instace.
+else
+        break
+fi
+done
+
+echo /dev/sd${newdiskname} is available for use.
+
+# Now lets create a new volume with that unused AWS device name
+VOLUME=$(/usr/local/bin/aws ec2 create-volume --availability-zone ${REGION} --size ${SIZE} --volume-type gp2 --tag-specifications "ResourceType=volume,Tags=[{Key=Name,Value=${newdiskname}}]" | grep \"VolumeId\"\:  | awk '{print $2}' | sed 's/[",]//g')
+
+# Lets wait for that to finish before moving on
+/usr/local/bin/aws ec2 wait volume-available --volume-ids ${VOLUME}
+
+# Now attach the new volume to me
+/usr/local/bin/aws ec2 attach-volume --volume-id ${VOLUME} --device /dev/sd${newdiskname} --instance-id ${INSTANCE}
+
+# Lets wait for that to finish before moving on
+/usr/local/bin/aws ec2 wait volume-in-use --volume-ids ${VOLUME}
+
+# Now we saw that sometimes it takes a second or two for a new OS device file to be created so lets wait 2 seconds
+sleep 2
+
+# Now we get an updated list of disks
+NEWDISKS=($(lsblk -a | grep -v ^NAME | grep -v ^'├' | grep -v ^'└' | awk '{print $1}'))
+
+# Now we just compare this new list with the original list and anything new must be our new disk
+NEWDEVICE=($(echo ${NEWDISKS[@]} ${CURRENTDISKS[@]} | tr ' ' '\n' | sort | uniq -u))
+
+echo ${NEWDEVICE[@]}
+
+# Lets just double check there is only one NEWDEVICE and if not lets exit as something isn't right
+if [[ `echo ${NEWDEVICE[@]} | awk '{print $2}'` == "" ]]
+then
+echo No second disk
+else echo More than one - panic
+exit
+fi
+
+# Now lets check if this new disk is unpartitioned before we partition it
+if [[ $(/sbin/sfdisk -d /dev/${NEWDEVICE} 2>/dev/null) == "" ]]
+then
+#echo "Device not partitioned"
+#/sbin/parted /dev/${NEWDEVICE} mklabel gpt --script
+#/sbin/parted /dev/${NEWDEVICE} mkpart primary 0% 100% --script
+echo No partition necessary for XFS
+# Assumine the use of xfs but could add lvm2 - if its installed - to do pvcreate etc
+mkfs.xfs /dev/${NEWDEVICE}
+mkdir $MOUNT
+cat >> /etc/fstab << EOF
+/dev/$NEWDEVICE	$MOUNT		xfs    defaults        1 2
+EOF
+mount -a
+else
+echo Disk seems to have a partition so existing rapidly
+exit
+fi
+
+}
+
+
+modify_existing_disk () {
+echo Under construction
+exit
+}
+
+
 case $1 in
 
   add)
@@ -21,13 +115,40 @@ case $1 in
 	exit
 	fi
 
-	ACTION=add
+        MOUNT=$3
+        if [[ $MOUNT == "" ]]
+        then
+        echo Need to give me a mount point name like /u01
+        exit
+        fi
 
+        if [[ "${MOUNT:0:1}" == "/" ]]
+        then
+        echo "Leading / found as expected"
+        else
+        echo "No leading / found and I am not adding one for you"
+        exit
+        fi
+
+	if [[ -d $MOUNT ]]
+	then
+	echo Mount already exists so I am stopping right here
+	exit
+	fi
+
+	grep -v ^# /etc/fstab | grep -v ^$ | awk '{print $2}' | grep ^${MOUNT}$ && {
+	echo Found that mount point already listed in fstab so stopping
+	exit
+	}
+
+###	ACTION=add
+	add_new_disk $SIZE $MOUNT
     ;;
 
   modify)
 	echo modify
-	ACTION=modify
+###	ACTION=modify
+	modify_existing_disk
 	exit
     ;;
 
@@ -38,53 +159,3 @@ case $1 in
 
 esac
 
-
-if [[ $ACTION == "add" ]]
-then
-
-CURRENTDISKS=($(lsblk -a | grep -v ^NAME | grep -v ^'├' | grep -v ^'└' | awk '{print $1}'))
-
-echo ${CURRENTDISKS[@]}
-
-INSTANCE=$(curl -q -s http://169.254.169.254/latest/meta-data/instance-id)
-REGION=$(curl -q -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-
-volumes=($(/usr/local/bin/aws ec2 describe-volumes --region ${REGION%?} --filters "Name=attachment.instance-id,Values=$INSTANCE" | grep \"Device\"\:  | awk '{print $2}' | sed 's/[",]//g'))
-
-echo ${volumes[*]}
-
-for newdiskname in f g h i j k l m n o p q r s t u v w x z y
-do
-
-if [[ " ${volumes[*]} " =~ " /dev/sd${newdiskname} " ]];
-then
-	echo /dev/sd${newdiskname} is already attached to this instace.
-else
-	break
-fi
-done
-
-echo /dev/sd${newdiskname} is available for use.
-
-VOLUME=$(/usr/local/bin/aws ec2 create-volume --availability-zone ${REGION} --size ${SIZE} --volume-type gp2 --tag-specifications "ResourceType=volume,Tags=[{Key=Name,Value=${newdiskname}}]" | grep \"VolumeId\"\:  | awk '{print $2}' | sed 's/[",]//g')
-
-/usr/local/bin/aws ec2 wait volume-available --volume-ids ${VOLUME}
-
-/usr/local/bin/aws ec2 attach-volume --volume-id ${VOLUME} --device /dev/sd${newdiskname} --instance-id ${INSTANCE}
-
-/usr/local/bin/aws ec2 wait volume-in-use --volume-ids ${VOLUME}
-
-sleep 2
-
-lsblk -a | grep -v ^NAME | grep -v ^'├' | grep -v ^'└' | awk '{print $1}'
-
-NEWDISKS=($(lsblk -a | grep -v ^NAME | grep -v ^'├' | grep -v ^'└' | awk '{print $1}'))
-
-echo ${NEWDISKS[@]}
-
-NEWDEVICE=($(echo ${NEWDISKS[@]} ${CURRENTDISKS[@]} | tr ' ' '\n' | sort | uniq -u))
-
-echo $NEWDEVICE
-
-
-fi
